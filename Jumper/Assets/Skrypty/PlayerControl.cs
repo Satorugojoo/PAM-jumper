9fd198dd1b1ad4456a5988bf10852d41ff734a0c
Satorugojoo <milosz.kaplanek@spoko.pl>
2023-06-13 18:49:53 +0200

    Dodanie plików aktualnego stanu projektu
    Stare zostały usunięte. Wstawiam najnowszą wersję.

---
+++ Jumper/Assets/Skrypty/PlayerControl.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControl : MonoBehaviour
+{
+    Rigidbody rb;
+    public float jumpForce;
+    bool canJump;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && canJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ziemia")
+        {
+            canJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ziemia")
+        {
+            canJump = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Przeszkoda")
+        {
+            SceneManager.LoadScene("Gra");
+        }
+    }
+}
